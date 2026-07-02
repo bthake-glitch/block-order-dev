f21b7240c410aafc4b11116ce5b38de1353d3d76
bthake-glitch <bthake@bigpond.com.au>
2026-07-02 19:58:29 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bt-block-order-dev-v5-15-total';
+const CACHE = 'bt-block-order-dev-v5-16-summary';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-yellow-192.png', './icon-yellow-512.png'];
 
 self.addEventListener('install', e => {
